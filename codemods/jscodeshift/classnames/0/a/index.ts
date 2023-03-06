b08dec7ad040b30df32aff81e9cc41ca6193a806
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-03-06 17:45:04 +0100

    wip

--- codemods/jscodeshift/classnames/0/a/index.ts
+++ codemods/jscodeshift/classnames/0/a/index.ts
@@ -1,4 +1,10 @@
-import type { FileInfo, API, Options, LogicalExpression } from 'jscodeshift';
+import type {
+	FileInfo,
+	API,
+	Options,
+	LogicalExpression,
+	Identifier,
+} from 'jscodeshift';
 
 type ExpressionKind = LogicalExpression['left'];
 
@@ -11,59 +17,105 @@ export default function transform(
 	const j = api.jscodeshift;
 	const root = j(file.source);
 
-	const ceCollection = root.find(j.CallExpression, {
+	root.find(j.CallExpression, {
 		type: 'CallExpression',
 		callee: {
 			type: 'Identifier',
 			name: 'ctl',
 		},
+	}).forEach((cePath) => {
+		const ceCollection = j(cePath);
+
+		const identifiers: Identifier[] = [];
+		const literals: string[] = [];
+		const objectExpressions: [ExpressionKind, ExpressionKind][] = [];
+
+		ceCollection.find(j.TemplateElement).forEach((tePath) => {
+			const templateElement = tePath.node;
+
+			const names = templateElement.value.raw
+				.split(/\s/)
+				.map((str) => str.trim())
+				.filter((str) => str !== '');
+
+			literals.push(...names);
+		});
+
+		ceCollection.find(j.TemplateLiteral).forEach((tlPath) => {
+			const templateLiteral = tlPath.node;
+
+			templateLiteral.expressions.forEach((expressionKind) => {
+				if (expressionKind.type === 'Identifier') {
+					identifiers.push(expressionKind);
+				}
+			});
+		});
+
+		ceCollection
+			.find(j.LogicalExpression, {
+				type: 'LogicalExpression',
+				operator: '&&',
+				right: {
+					type: 'StringLiteral',
+				},
+			})
+			.forEach((lePath) => {
+				const { left, right } = lePath.node;
+
+				objectExpressions.push([left, right]);
+			});
+
+		ceCollection.replaceWith(
+			j.expressionStatement(
+				j.callExpression(j.identifier('cn'), [
+					...identifiers,
+					...literals.map((e) => j.literal(e)),
+					...objectExpressions.map(([left, right]) => ({
+						type: 'ObjectExpression' as const,
+						properties: [
+							{
+								type: 'ObjectProperty' as const,
+								key: right,
+								value: left,
+							},
+						],
+					})),
+				]),
+			),
+		);
 	});
 
-	const elements: string[] = [];
-	const objectExpressions: [ExpressionKind, ExpressionKind][] = [];
-
-	ceCollection.find(j.TemplateElement).forEach((TemplateElementPath) => {
-		const templateElement = TemplateElementPath.node;
-
-		const names = templateElement.value.raw
-			.split(/\s/)
-			.map((s) => s.trim())
-			.filter((x) => x !== '');
-
-		elements.push(...names);
-	});
-
-	root.find(j.LogicalExpression, {
-		type: 'LogicalExpression',
-		operator: '&&',
-		right: {
+	root.find(j.ImportDeclaration, {
+		type: 'ImportDeclaration',
+		importKind: 'value',
+		specifiers: [
+			{
+				type: 'ImportDefaultSpecifier',
+				local: {
+					type: 'Identifier',
+					name: 'ctl',
+				},
+			},
+		],
+		source: {
 			type: 'StringLiteral',
+			extra: {
+				rawValue: '@netlify/classnames-template-literals',
+				raw: "'@netlify/classnames-template-literals'",
+			},
+			value: '@netlify/classnames-template-literals',
 		},
-	}).forEach((lePath) => {
-		const le = lePath.node;
-
-		le.left;
-
-		objectExpressions.push([le.left, le.right]);
+	}).replaceWith({
+		type: 'ImportDeclaration',
+		importKind: 'value',
+		specifiers: [
+			{
+				type: 'ImportDefaultSpecifier',
+				local: { type: 'Identifier', name: 'cn' },
+			},
+		],
+		source: { type: 'StringLiteral', value: 'classnames' },
 	});
-
-	ceCollection.replaceWith(
-		j.expressionStatement(
-			j.callExpression(j.identifier('cn'), [
-				...elements.map((e) => j.literal(e)),
-				...objectExpressions.map(([left, right]) => ({
-					type: 'ObjectExpression' as const,
-					properties: [
-						{
-							type: 'ObjectProperty' as const,
-							key: right,
-							value: left,
-						},
-					],
-				})),
-			]),
-		),
-	);
 
 	return root.toSource();
 }
