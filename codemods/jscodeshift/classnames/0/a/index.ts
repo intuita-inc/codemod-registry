d5ff6c358ebb6e25b80d6ee6264edda4f9e3a02f
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-03-07 13:45:29 +0100

    wip

--- codemods/jscodeshift/classnames/0/a/index.ts
+++ codemods/jscodeshift/classnames/0/a/index.ts
@@ -1,221 +1,187 @@
 import type {
-    FileInfo,
-    API,
-    Options,
-    LogicalExpression,
-    TemplateLiteral,
+	FileInfo,
+	API,
+	Options,
+	LogicalExpression,
+	TemplateLiteral,
 	StringLiteral,
 	Literal,
+	Identifier,
 } from 'jscodeshift';
 
 type ExpressionKind = LogicalExpression['left'];
 
-type ObjectExpression = {left: ExpressionKind, right: (StringLiteral | Literal) }
+type ObjectExpression = {
+	left: ExpressionKind;
+	right: StringLiteral | Literal | Identifier;
+};
 
 const getLiteralsFromTemplateLiteral = (
-    templateLiteral: TemplateLiteral,
+	templateLiteral: TemplateLiteral,
 ): ReadonlyArray<string> => {
-    return templateLiteral.quasis.flatMap((templateElement) => {
-        return templateElement.value.raw
-            .split(/\s/)
-            .map((str) => str.trim())
-            .filter((str) => str !== '');
-    });
+	return templateLiteral.quasis.flatMap((templateElement) => {
+		return templateElement.value.raw
+			.split(/\s/)
+			.map((str) => str.trim())
+			.filter((str) => str !== '');
+	});
 };
 
 // this is the entry point for a JSCodeshift codemod
 export default function transform(
-    file: FileInfo,
-    api: API,
-    options: Options,
+	file: FileInfo,
+	api: API,
+	options: Options,
 ): string | undefined {
-    const j = api.jscodeshift;
-    const root = j(file.source);
+	const j = api.jscodeshift;
+	const root = j(file.source);
 
-    let dirtyFlag = false;
+	let dirtyFlag = false;
 
-    root.find(j.CallExpression, {
-        type: 'CallExpression',
-        callee: {
-            type: 'Identifier',
-            name: 'ctl',
-        },
-    }).forEach((cePath) => {
-        const ceCollection = j(cePath);
+	root.find(j.CallExpression, {
+		type: 'CallExpression',
+		callee: {
+			type: 'Identifier',
+			name: 'ctl',
+		},
+	}).forEach((cePath) => {
+		const ceCollection = j(cePath);
 
-        const handleTemplateLiteral = (templateLiteral: TemplateLiteral) => {
-            const literals = getLiteralsFromTemplateLiteral(templateLiteral);
+		const handleTemplateLiteral = (templateLiteral: TemplateLiteral) => {
+			const identifiers: Identifier[] = [];
 
-            const objectExpressions = templateLiteral.expressions.flatMap((expression): ObjectExpression[] => {
-                if (expression.type === 'LogicalExpression') {
-                    if (expression.right.type === 'TemplateLiteral') {
-                        //handleTemplateLiteral(expression.right);
+			const literals = getLiteralsFromTemplateLiteral(templateLiteral);
 
-                        return getLiteralsFromTemplateLiteral(expression.right)
-                            .map((name) => {
-                                return ({
+			const objectExpressions = templateLiteral.expressions.flatMap(
+				(expression): ObjectExpression[] => {
+					if (expression.type === 'Identifier') {
+						identifiers.push(expression);
+					}
+
+					if (expression.type === 'LogicalExpression') {
+						if (expression.right.type === 'TemplateLiteral') {
+							const returns = handleTemplateLiteral(
+								expression.right,
+							);
+
+							return [
+								...returns.identifiers.map((identifier) => {
+									return {
+										left: expression.left,
+										right: identifier,
+									};
+								}),
+								...returns.literals.map((name) => {
+									return {
+										left: expression.left,
+										right: j.literal(name),
+									};
+								}),
+								...returns.objectExpressions.map(
+									({ left, right }) => {
+										return {
+											left: j.logicalExpression(
+												'&&',
+												left,
+												expression.left,
+											),
+											right,
+										};
+									},
+								),
+							];
+						}
+
+						if (expression.right.type === 'StringLiteral') {
+							return [
+								{
 									left: expression.left,
-                                    right: j.literal(name),
-								});
-                            });
-                    }
+									right: expression.right,
+								},
+							];
+						}
+					}
 
-                    if (expression.right.type === 'StringLiteral') {
-                        return [(
-							{
-								left: expression.left,
-								right: expression.right,
-							}
-                        )];
-                    }
-                }
-
-				return [];
-            });
+					return [];
+				},
+			);
 
 			return {
+				identifiers,
 				literals,
 				objectExpressions,
-			}
-        };
+			};
+		};
 
-        const literals: string[] = [];
-        const objectExpressions: ObjectExpression[] = [];
+		const identifiers: Identifier[] = [];
+		const literals: string[] = [];
+		const objectExpressions: ObjectExpression[] = [];
 
-        cePath.node.arguments.forEach((argument) => {
-            if (argument.type === 'TemplateLiteral') {
-                const returns = handleTemplateLiteral(argument);
+		cePath.node.arguments.forEach((argument) => {
+			if (argument.type === 'TemplateLiteral') {
+				const returns = handleTemplateLiteral(argument);
 
+				identifiers.push(...returns.identifiers);
 				literals.push(...returns.literals);
 				objectExpressions.push(...returns.objectExpressions);
-            }
-        });
+			}
+		});
 
-        ceCollection.replaceWith(() => {
-            dirtyFlag = true;
+		ceCollection.replaceWith(() => {
+			dirtyFlag = true;
 
-            return j.callExpression(j.identifier('cn'), [
-                ...literals.map((e) => j.literal(e)),
-                ...objectExpressions.map(( { left, right }) => ({
-                    type: 'ObjectExpression' as const,
-                    properties: [
-                        {
-                            type: 'ObjectProperty' as const,
-                            key: right,
-                            value: left,
-                        },
-                    ],
-                })),
-            ]);
-        });
-    });
+			return j.callExpression(j.identifier('cn'), [
+				...identifiers,
+				...literals.map((e) => j.literal(e)),
+				...objectExpressions.map(({ left, right }) => ({
+					type: 'ObjectExpression' as const,
+					properties: [
+						{
+							type: 'ObjectProperty' as const,
+							key: right,
+							value: left,
+						},
+					],
+				})),
+			]);
+		});
+	});
 
-    root.find(j.ImportDeclaration, {
-        type: 'ImportDeclaration',
-        importKind: 'value',
-        specifiers: [
-            {
-                type: 'ImportDefaultSpecifier',
-                local: {
-                    type: 'Identifier',
-                    name: 'ctl',
-                },
-            },
-        ],
-        source: {
-            type: 'StringLiteral',
-            value: '@netlify/classnames-template-literals',
-        },
-    }).replaceWith(() => {
-        dirtyFlag = true;
+	root.find(j.ImportDeclaration, {
+		type: 'ImportDeclaration',
+		importKind: 'value',
+		specifiers: [
+			{
+				type: 'ImportDefaultSpecifier',
+				local: {
+					type: 'Identifier',
+					name: 'ctl',
+				},
+			},
+		],
+		source: {
+			type: 'StringLiteral',
+			value: '@netlify/classnames-template-literals',
+		},
+	}).replaceWith(() => {
+		dirtyFlag = true;
 
-        return {
-            type: 'ImportDeclaration',
-            importKind: 'value',
-            specifiers: [
-                {
-                    type: 'ImportDefaultSpecifier',
-                    local: { type: 'Identifier', name: 'cn' },
-                },
-            ],
-            source: { type: 'StringLiteral', value: 'classnames' },
-        };
-    });
+		return {
+			type: 'ImportDeclaration',
+			importKind: 'value',
+			specifiers: [
+				{
+					type: 'ImportDefaultSpecifier',
+					local: { type: 'Identifier', name: 'cn' },
+				},
+			],
+			source: { type: 'StringLiteral', value: 'classnames' },
+		};
+	});
 
-    if (!dirtyFlag) {
-        return undefined;
-    }
+	if (!dirtyFlag) {
+		return undefined;
+	}
 
-    return root.toSource();
-}
-
-                }
-            });
-        };
-
-        const literals: string[] = [];
-        const objectExpressions: [ExpressionKind, ExpressionKind][] = [];
-
-        cePath.node.arguments.forEach((argument) => {
-            if (argument.type === 'TemplateLiteral') {
-                handleTemplateLiteral(argument);
-            }
-        });
-
-        ceCollection.replaceWith(() => {
-            dirtyFlag = true;
-
-            return j.callExpression(j.identifier('cn'), [
-                ...literals.map((e) => j.literal(e)),
-                ...objectExpressions.map(([left, right]) => ({
-                    type: 'ObjectExpression' as const,
-                    properties: [
-                        {
-                            type: 'ObjectProperty' as const,
-                            key: right,
-                            value: left,
-                        },
-                    ],
-                })),
-            ]);
-        });
-    });
-
-    root.find(j.ImportDeclaration, {
-        type: 'ImportDeclaration',
-        importKind: 'value',
-        specifiers: [
-            {
-                type: 'ImportDefaultSpecifier',
-                local: {
-                    type: 'Identifier',
-                    name: 'ctl',
-                },
-            },
-        ],
-        source: {
-            type: 'StringLiteral',
-            value: '@netlify/classnames-template-literals',
-        },
-    }).replaceWith(() => {
-        dirtyFlag = true;
-
-        return {
-            type: 'ImportDeclaration',
-            importKind: 'value',
-            specifiers: [
-                {
-                    type: 'ImportDefaultSpecifier',
-                    local: { type: 'Identifier', name: 'cn' },
-                },
-            ],
-            source: { type: 'StringLiteral', value: 'classnames' },
-        };
-    });
-
-    if (!dirtyFlag) {
-        return undefined;
-    }
-
-    return root.toSource();
+	return root.toSource();
 }
